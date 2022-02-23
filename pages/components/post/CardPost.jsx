b9da2813799e14700193b1cdd782f9cb8b19e230
Kevin <kevinator900@gmail.com>
2022-02-23 15:28:42 -0700

    made progress on posts

--- pages/components/post/CardPost.jsx
+++ pages/components/post/CardPost.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Card,
   Icon,
@@ -18,15 +18,24 @@ import ImageModal from './ImageModal'
 import NoImageModal from './NoImageModal'
 import calculateTime from '../../util/calculateTime'
 import { deletePost, likePost } from '../../util/postActions'
+import { getIcon } from '../../util/getIcon'
 
 const CardPost = ({ post, user, setPosts, setShowToastr }) => {
 
+  const [icon, setIcon] = useState(getIcon())
+  
+  
+  
   const [likes, setLikes] = useState(post.likes)
   const isLiked = likes.find( (like) => like.user === user._id)
-
+  
   const [comments, setComments] = useState(post.comments)
   const [error, setError] = useState(null)
   const [showModal, setShowModal] = useState(false)
+  
+  useEffect(() => {
+    setIcon(getIcon())
+  }, [isLiked])
 
   const addPropsToModal = () => ({
     post,
@@ -59,6 +68,91 @@ const CardPost = ({ post, user, setPosts, setShowToastr }) => {
             avatar
             circular
           />
+          {(user.role === 'admin' || post.user._id === user._id) && (
+            <>
+              <Popup 
+                on={'click'}
+                position="top right"
+                trigger={<Image 
+                    src='/deleteIcon.svg'
+                    style={{cursor: 'pointer'}}
+                    size="mini"
+                    floated="right"
+                  />
+                }
+              >
+                <Header as={'h4'} content="Are you sure?"/>
+                <p>This action is irreversable</p>
+                
+                <Button 
+                  color="red"
+                  icon="trash"
+                  content="Delete"
+                  onClick={() => deletePost(post._id, setPosts, setShowToastr)}
+                />
+              </Popup>
+            </>
+          )}
+
+          <Card.Header>
+            <Link href={`/${post.user.username}`}>
+              <a>{post.user.name}</a>
+            </Link>
+          </Card.Header>
+
+          <Card.Meta>{calculateTime(post.createdAt)}</Card.Meta>
+          {post.location && <Card.Meta
+            content={post.location}
+          />}
+
+          <Card.Description
+            style={{ fontSize: '1.5rem', letterSpacing: '0.2px'}}
+          >{post.text}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <Icon 
+              name={icon}
+              color={isLiked ? 'red' : undefined}
+              style={{ cursor: 'pointer' }}
+              onClick={() => likePost(post._id, user._id, setLikes, !isLiked)}
+            />
+            <LikesList 
+              postId={post._id}
+              trigger={
+                likes.length > 0 && (
+                  <span className="spanLikesList">
+                    {`${likes.length} ${likes.length === 1 ? 'like' : 'likes'}`}
+                  </span>
+                )
+              }
+            />
+            <Icon 
+              name="comment outline"
+              style={{ marginLeft: '7px' }}
+              color='blue'
+            />
+            {comments.length && (
+              comments.map((comment, i) => (
+                (i < 3) && (
+                  <PostComments 
+                    key={comment._id}
+                    comment={comment}
+                    postId={post._id}
+                    user={user}
+                    setComments={setComments}
+                  />
+                )
+              ))
+            )}
+            {comments.length > 3 && (
+              <Button 
+                content="View More"
+                color='orange'
+                basic
+                circular
+                onClick={() => setShowModal(true)}
+              />
+            )}
         </Card.Content>
       </Card>
     </Segment>
--- pages/util/calculateTime.js
+++ pages/util/calculateTime.js
@@ -1,3 +1,18 @@
-const calculateTime = () => { }
+import moment from 'moment'
+import Moment from 'react-moment'
+
+const calculateTime = (createdAt) => {
+  const today = moment(Date.now())
+  const postDate = moment(createdAt)
+  const diffInHours = today.diff(postDate, 'hours')
+
+  if(diffInHours < 48) {
+    return <>
+      {diffInHours < 24 ? 'Today' : 'Yesterday'} At <Moment format='hh:mm A'>{createdAt}</Moment>
+    </>
+  } else {
+    return <Moment format="MM/DD/YYYY hh:mm A">{createdAt}</Moment>
+  }
+}
 
 export default calculateTime;
