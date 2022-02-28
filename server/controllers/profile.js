1daef5376eb719a624b7ef260eb83774acf67827
Kevin <kevinator900@gmail.com>
2022-02-28 15:27:34 -0700

    started profile controllers

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,5 +1,101 @@
-const UserModel = require('../models/UserModel')
-const PostModel = require('../models/PostModel')
-const FollowerModel = require('../models/FollowerModel')
-const ProfileModel = require('../models/ProfileModel')
-const bcrypt = require('bcryptjs')
+const UserModel = require('../models/UserModel');
+const PostModel = require('../models/PostModel');
+const FollowerModel = require('../models/FollowerModel');
+const ProfileModel = require('../models/ProfileModel');
+const bcrypt = require('bcryptjs');
+
+const getProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await UserModel.findOne({ username: username.toLowerCase() });
+    if (!user) return res.status(404).send('No User Found');
+
+    const profile = await ProfileModel.findOne({ user: user._id }).populate(
+      'user'
+    );
+
+    const profileFollowStats = await FollowerModel.findOne({ user: user._id });
+    return res.status(200).json({
+      profile,
+      followersLength:
+        profileFollowStats.followers.length > 0
+          ? profileFollowStats.followers.length
+          : '0',
+      followingLength:
+        profileFollowStats.following.length > 0
+          ? profileFollowStats.following.length
+          : '0',
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ getProfile');
+  }
+};
+
+const getUserPosts = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ getUserPosts');
+  }
+};
+
+const getFollowers = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ getFollowers');
+  }
+};
+
+const getFollowing = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ getFollowing');
+  }
+};
+
+const followUser = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ followUser');
+  }
+};
+
+const unfollowUser = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ unfollowUser');
+  }
+};
+
+const updateProfile = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ updateProfile');
+  }
+};
+
+const updatePassword = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('error @ updatePassword');
+  }
+};
+
+module.exports = {
+  getProfile,
+  getUserPosts,
+  getFollowers,
+  getFollowing,
+  followUser,
+  unfollowUser,
+  updatePassword,
+  updateProfile,
+};
--- server/routes/profileRoute.js
+++ server/routes/profileRoute.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const {
+  getProfile,
+  getUserPosts,
+  getFollowers,
+  getFollowing,
+  followUser,
+  unfollowUser,
+  updatePassword,
+  updateProfile,
+} = require('../controllers/profile');
 
-
+router.route('/:username').get(getProfile);
 
 module.exports = router;
